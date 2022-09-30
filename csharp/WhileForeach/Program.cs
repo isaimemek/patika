353a4c45c90eeff56ef3fe2b130151026386d621
isa imemek <isaimemek@hotmail.com>
2022-09-30 16:03:58 +0300

    While and Foreach

---
+++ csharp/WhileForeach/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        int counter = 1;
+        int total = 0;
+        while(counter <= num)
+        {
+            total += counter;
+            counter ++;
+        }
+        Console.WriteLine(total/num);
+
+        char character = 'a';
+        while (character < 'z')
+        {
+            Console.Write(character);
+            character ++;
+        }
+
+        Console.Write("***** Foreach *****");
+        string[] cars = {"BMW", "Ford", "Toyota", "Nissan"};
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+    }
+}
